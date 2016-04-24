61692ea4c6d8f7ff5ea426493738c6dd2e86fac5
Barbarossa8 <antoine.denauw@student.uclouvain.be>
2016-04-24 18:40:45 +0200

    Commentaires des fonctions

--- Code/PageRankCode/PageRank.java
+++ Code/PageRankCode/PageRank.java
@@ -10,10 +10,11 @@ public class PageRank
     /**
      * Main utilisé pour des tests
      */
-    public static void main()
-    {
-        float[][] matrice = { {1,2},{8,4}};
+    public static void main(String[] args) {
+
+        float[][] matrice = { {1,2},{3,4},{5,6},{7,8} };
         float[][] matrice2 = { {5,6},{7,8} };
+
         print_a_matrix(matrice);    //Imprime la matrice
         print_a_matrix(transpose(matrice)); //Imprime la meme matrice mais en transpose
         print_a_vector(degre(matrice));// Imprime et calcule le degre de la matrice de base
@@ -29,43 +30,40 @@ public class PageRank
      * @post: Un vecteur x contenant les scores d’importance des noeuds ordonnés 
      * 		  dans le même ordre que la matrice d’adjacence.
      */
-    public float[] calcul (float matrice[][]){
+    public float[] calcul (float matrice[][]) {
         return null;
-
     }
 
     /**
-     * Method transpose
+     * Méthode transpose.
      *
-     * @param matrice A parameter
-     * @return The return value
+     * @pre Une matrice non-nulle.
+     * @post Retourne la matrice transposée.
      */
-    public static float [][] transpose(float [][] matrice)
-    {
+    public static float [][] transpose(float [][] matrice) {
         float [][] matrice_transpose = new float [matrice.length] [matrice.length]; 
 
-        for(int runner = 0 ; runner < matrice.length ; runner++)
-        {
-            for(int runner2 = 0 ; runner2 < matrice.length ; runner2++)
-            {
+        for(int runner = 0 ; runner < matrice.length ; runner++) {
+            for(int runner2 = 0 ; runner2 < matrice.length ; runner2++) {
                 matrice_transpose[runner2][runner] = matrice[runner][runner2];
             }
         }
+
         return matrice_transpose;
     }
 
     /**
-     * Method multiply
+     * Méthode multiplication.
      *
-     * @param matrice_A A parameter
-     * @param matrice_B A parameter
-     * @return The return value
+     * @pre Prend en argument deux matrices non-nulles.
+     * @post Retourne le résultat de la multiplication des deux matrices.
      */
     public static float[][] multiply(float[][] matrice_A, float[][] matrice_B) {
         int rows_A = matrice_A.length;
         int columns_A = matrice_A [0].length; 
         int columns_B = matrice_B[0].length;
         float[][] matrice_AB = new float[rows_A][columns_B];
+
         for (int i = 0; i < rows_A; i++) {
             for (int j = 0; j < columns_B; j++) {
                 for (int k = 0; k < columns_A; k++) {
@@ -73,42 +71,41 @@ public class PageRank
                 }
             }
         }
+
         return matrice_AB;
     }
 
     /**
-     * Method degre
+     * Méthode degré.
      *
-     * @param matrice A parameter
-     * @return The return value
+     * @pre Prends en argument une matrice A non-nulle.
+     * @post Retourne le degré de cette matrice.
      */
-    public static float[] degre(float[][] matrice)
-    {
+    public static float[] degre(float[][] matrice) {
         float[] matrice_degre=new float [matrice.length]; //On cree la matrice qui contiendra les degre
-        for ( int runner_1=0 ; runner_1 < matrice.length ; runner_1++)
-        {
+
+        for ( int runner_1=0 ; runner_1 < matrice.length ; runner_1++) {
             float sum=0; //Stockera le degre de la ligne runner_1
-            for ( int runner_2=0 ; runner_2 < matrice.length ; runner_2++)
-            {
+            for ( int runner_2=0 ; runner_2 < matrice.length ; runner_2++) {
                 sum+=matrice[runner_1][runner_2];
             }
             matrice_degre [runner_1] = sum;
         }
+
         return matrice_degre;
     }
 
     /**
-     * Method print_a_matrix
+     * Méthode qui imprime une matrice.
      *
-     * @param matrice A parameter
+     * @pre Prends en argument une matrice non-nulle.
+     * @post Imprime la matrice sous la sortie standrad.
      */
-    public static void print_a_matrix(float[][] matrice)
-    {
+    public static void print_a_matrix(float[][] matrice) {
         System.out.println("\n");
-        for(int runner = 0 ; runner < matrice.length ; runner++)
-        {
-            for(int runner2 = 0 ; runner2 < matrice.length ; runner2++)
-            {
+
+        for(int runner = 0 ; runner < matrice.length ; runner++) {
+            for(int runner2 = 0 ; runner2 < matrice.length ; runner2++) {
                 System.out.print(" " + matrice[runner][runner2]);
             }
             System.out.println("");
@@ -116,17 +113,16 @@ public class PageRank
     }
 
     /**
-     * Method print_a_matrix
+     * Méthode qui imprime un vecteur.
      *
-     * @param matrice A parameter
+     * @pre Prends en argument un vecteur non-null.
+     * @post Imprime le vecteur sous la sortie standard.
      */
-    public static void print_a_vector(float[] vector)
-    {
+    public static void print_a_vector(float[] vector) {
         System.out.println("\n");
-        for(int runner = 0 ; runner < vector.length ; runner++)
-        {
+
+        for(int runner = 0 ; runner < vector.length ; runner++) {
             System.out.println(vector[runner]);
         }
     }
-
 }
